3ed1dcd7f15d29ef410cac112baeee9087a01f35
midzer <midzer@gmail.com>
2017-04-15 20:19:37 +0200

    move themeswitch to navbar

--- js/navbar.js
+++ js/navbar.js
@@ -9,31 +9,30 @@ fuzzy: false,
 exclude: ['Welcome']
 });
 
-function switchTheme() {
-    var newTheme;
-    if (localStorage.getItem('theme') == 'light') {
-        newTheme = 'dark';
-    }
-    else {
-        newTheme = 'light';
-    }
-    localStorage.setItem('theme', newTheme);
-}
-
-function setIcon() {
-    var icon;
-    if (localStorage.getItem('theme') == 'light') {
-        icon = 'moon';
-    }
-    else {
+function setTheme(local) {
+    var theme, icon, css;
+    if (local == 'light') {
+        theme = 'dark';
         icon = 'sun';
+        css = '/css/dark-theme.min.css';
     }
+    else  {
+        theme = 'light';
+        icon = 'moon';
+        css = ''
+    }
+    localStorage.setItem('theme', theme);
     document.getElementById('theme-icon').setAttribute('class', 'icon-' + icon);
+    document.getElementById('theme-link').setAttribute('href', css);
 }
 
 document.getElementById('theme-switch').onclick = function() {
-    switchTheme();
-    setTheme();
-    setIcon();
+    setTheme(localStorage.getItem('theme'));
 };
-setIcon();
+
+if (localStorage.getItem('theme') == 'dark') {
+    setTheme('light');
+}
+else {
+    localStorage.setItem('theme', 'light');
+}
