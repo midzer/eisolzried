353bee05f7caeaabe9b45e99534ba277dd255a7c
midzer <midzer@gmail.com>
2017-05-16 22:19:58 +0200

    introduce experimental image gallery lazy loading via Intersection Observer

--- js/main.js
+++ js/main.js
@@ -1,1 +1,31 @@
 $('.gallery a').simpleLightbox();
+
+// 5. Convert node list of all images with data-src attributed to array
+var imgs = document.querySelectorAll('img[data-src]');
+if (! ('IntersectionObserver' in window)) {
+  console.log('Intersection Observers not supported');
+  for (var i = 0; i < imgs.length; i++) {
+      imgs[i].src = imgs[i].dataset.src;
+  }
+} else {
+  // 1. Create the IntersectionObserver and bind it to the function we want it to work with
+  var observer = new IntersectionObserver(onChange, {
+      threshold: 0.5
+  });
+  
+  function onChange(changes) {
+      // 2. For each image that we want to change
+      changes.forEach(change => {
+          if (change.isIntersecting) {
+              console.log("loaded");
+              // 3. take image url from `data-src` attribute
+              change.target.src = change.target.dataset.src;
+              // 4. Stop observing the current target
+              observer.unobserve(change.target);
+              change.target.onload = () => change.target.classList.add('loaded');
+          }
+    })
+  }
+  // 6. Observe each image derived from the array above
+  imgs.forEach(img => observer.observe(img));
+}
