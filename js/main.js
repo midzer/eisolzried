8dde543ae2a66d9dc5baae7e44790ac9d2ca7912
midzer <midzer@gmail.com>
2017-05-18 16:12:09 +0200

    lazy load videos, too

--- js/main.js
+++ js/main.js
@@ -1,31 +1,50 @@
-// 5. Convert node list of all images with data-src attributed to array
-var imgs = document.querySelectorAll('img[data-src]');
+function replaceSrc(element) {
+    if (element.nodeName == 'VIDEO') {
+        var sources = element.getElementsByTagName('source');
+        for (let i = 0; i < sources.length; i++) {
+            sources[i].src = sources[i].dataset.src;
+            sources[i].removeAttribute('data-src');
+        }
+        element.load();
+    }
+    else {
+        element.src = element.dataset.src;
+        element.removeAttribute('data-src');
+        element.onload = function() {
+            element.classList.add('loaded');
+        }
+    }
+}
+
+// 1. Convert node list of all elements with data-src attributed to array
+var els = document.querySelectorAll('.lazy');
 if (! ('IntersectionObserver' in window)) {
   console.log('Intersection Observers not supported');
-  for (var i = 0; i < imgs.length; i++) {
-      imgs[i].src = imgs[i].dataset.src;
+  for (var i = 0; i < els.length; i++) {
+      replaceSrc(els[i]);
   }
 } else {
-  // 1. Create the IntersectionObserver and bind it to the function we want it to work with
+  // 2. Create the IntersectionObserver and bind it to the function we want it to work with
   var observer = new IntersectionObserver(onChange, {
       threshold: 0.5
   });
   
   function onChange(changes) {
-      // 2. For each image that we want to change
+      // 3. For each element that we want to change
       changes.forEach(change => {
           if (change.isIntersecting) {
-              console.log("loaded");
-              // 3. take image url from `data-src` attribute
-              change.target.src = change.target.dataset.src;
-              // 4. Stop observing the current target
+              // 4. take url from `data-src` attribute
+              replaceSrc(change.target);
+
+              // 5. Stop observing the current target
               observer.unobserve(change.target);
-              change.target.onload = () => change.target.classList.add('loaded');
+
+              console.log('lazy loaded element');
           }
-    })
+        })
   }
   // 6. Observe each image derived from the array above
-  imgs.forEach(img => observer.observe(img));
+  els.forEach(el => observer.observe(el));
 }
 
 $('.gallery a').simpleLightbox();
