1a5ef98cab86cde576d0e330f0876b784bfe5c2d
midzer <midzer@gmail.com>
2017-07-07 12:09:28 +0200

    remove GPU draining animation for lazy loaded elements

--- _assets/js/main.js
+++ _assets/js/main.js
@@ -14,7 +14,7 @@ if (els.length > 0) {
     function lazyLoad(els) {
         // 2. Create the IntersectionObserver and bind it to the function we want it to work with
         var observer = new IntersectionObserver(onChange, {
-            threshold: 0.25
+            rootMargin: '200px 0px'
         });
 
         function replaceSrc(element) {
