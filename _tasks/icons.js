8434f9cce3b6cb51bdd939440a709c0227523cb7
midzer <midzer@gmail.com>
2017-08-25 23:09:18 +0200

    fix referencing external SVG sprites

--- _tasks/icons.js
+++ _tasks/icons.js
@@ -7,17 +7,12 @@ import plumber from 'gulp-plumber';
 
 const config = {
   mode: {
-    inline: true,
     symbol: { // symbol mode to build the SVG
       dest: 'icons', // destination folder
       sprite: 'sprite.svg', // sprite name
-      example: false // do not build sample page
     }
   },
-  svg: {
-    xmlDeclaration: false, // strip out the XML attribute
-    doctypeDeclaration: false // don't include the !DOCTYPE declaration
-  },
+  
   shape				: {
 		transform		: [
 			{}
