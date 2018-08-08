349ecd08c6c9f0627e9cf4d533f12be13cc903b0
midzer <midzer@gmail.com>
2018-08-08 23:23:19 +0200

    improve script for video poster creation

--- _scripts/video_poster-creation.sh
+++ _scripts/video_poster-creation.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-mogrify  -format jpg -path thumbs *.mp4[25]
+for i in *.mp4; do ffmpeg -i "$i" -vf "scale='if(gt(dar,320/320),320*dar,320)':'if(gt(dar,320/320),320,320/dar)',setsar=1,crop=320:320" -frames:v 1 "${i%.*}.jpg"; done
