56f1d1e3e34a6f745b2030d251f09da35ba87f41
midzer <midzer@gmail.com>
2018-08-22 18:59:50 +0200

    get rid of IE11 polyfill as it's not working anyway and I can't be arsed to find ways to solve this issue

--- _tasks/webpack.prod.js
+++ _tasks/webpack.prod.js
@@ -2,10 +2,5 @@ const merge = require('webpack-merge');
 const common = require('./webpack.common.js');
 
 module.exports = merge(common, {
-    mode: 'production',
-    entry: {
-        polyfill: [
-          'babel-polyfill'
-        ]
-    }
+    mode: 'production'
 });
