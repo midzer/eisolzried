6d8534012a57f4e174182212cbd618ba69ae2514
midzer <midzer@gmail.com>
2018-08-27 11:17:19 +0200

    replace chartist by frappe-charts

--- _assets/js/charts.js
+++ _assets/js/charts.js
@@ -1,20 +1,34 @@
 'use strict'
 
-import 'chartist-plugin-legend'
-import Chartist from 'chartist'
+import { Chart } from 'frappe-charts/dist/frappe-charts.esm.js'
 
-Chartist.Bar('#member', {
-  labels: ['1990', '1995', '2000', '2005', '2010', '2015'],
-  series: [
-    { 'name': 'Aktive', 'data': [26, 27, 39, 41, 34, 38] },
-    { 'name': 'Jugend', 'data': [0, 2, 5, 2, 4, 4] },
-    { 'name': 'Ehrenmitglieder', 'data': [0, 0, 0, 1, 1, 3] },
-    { 'name': 'Passive', 'data': [2, 3, 8, 8, 12, 16] },
-    { 'name': 'Ausgetreten', 'data': [0, 0, 0, 6, 7, 3] }
-  ]
-},
-{
-  plugins: [
-    Chartist.plugins.legend()
-  ]
-})
+const chart = new Chart('#member', {
+  data: {
+    labels: ['1990', '1995', '2000', '2005', '2010', '2015'],
+
+	  datasets: [
+		  {
+	  		name: 'Aktive', chartType: 'bar',
+	  		values: [26, 27, 39, 41, 34, 38]
+	  	},
+  		{
+  			name: 'Jugend', chartType: 'bar',
+  			values: [0, 2, 5, 2, 4, 4]
+  		},
+  		{
+  			name: 'Ehrenmitglieder', chartType: 'bar',
+  			values: [0, 0, 0, 1, 1, 3]
+      },
+      {
+	  		name: 'Passive', chartType: 'bar',
+	  		values: [2, 3, 8, 8, 12, 16]
+      },
+      {
+  			name: 'Ausgetreten', chartType: 'bar',
+  			values: [0, 0, 0, 6, 7, 3]
+  		}
+    ],
+  },
+	title: 'Mitglieder Statistik',
+	type: 'bar'
+});
