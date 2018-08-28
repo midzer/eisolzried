1c1bbeaa7acf1ca8439c07c35efc647979cc7bc5
midzer <midzer@gmail.com>
2018-08-29 00:08:07 +0200

    refactor kuebelspritze game

--- _assets/js/kuebelspritze.js
+++ _assets/js/kuebelspritze.js
@@ -2,29 +2,24 @@
 
 import { toggleAudio } from './helper/toggleaudio'
 
-function hubDone (nextHub) {
-  kuebel.src = `/assets/images/games/kuebel-${nextHub}.png`
-  huebe.textContent = parseInt(huebe.textContent) + 1
-}
-
-const kuebel = document.getElementById('kuebel')
-const huebe = document.getElementById('huebe')
-
 if ('LinearAccelerationSensor' in window) {
   const sensor = new LinearAccelerationSensor()
   let nextHub = 'up'
-  const upPlayer = document.getElementById('kuebel-up-player')
-  const downPlayer = document.getElementById('kuebel-down-player')
   sensor.onreading = () => {
     if (sensor.z < -5 && nextHub === 'up') {
       hubDone(nextHub)
-      toggleAudio(upPlayer)
+      toggleAudio(document.getElementById('kuebel-up-player'))
       nextHub = 'down'
     } else if (sensor.z > 5 && nextHub === 'down') {
       hubDone(nextHub)
-      toggleAudio(downPlayer)
+      toggleAudio(document.getElementById('kuebel-down-player'))
       nextHub = 'up'
     }
+  }
+  var huebe = 0
+  function hubDone (nextHub) {
+    document.getElementById('kuebel').src = `/assets/images/games/kuebel-${nextHub}.png`
+    document.getElementById('huebe').textContent = ++huebe
   }
   sensor.start()
 }
