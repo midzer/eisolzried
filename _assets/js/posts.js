e9c1f57cb86e5f8bf1da0483ce559a3531f0a3de
midzer <midzer@gmail.com>
2018-09-04 14:20:31 +0200

    improve posts list by requestAnimationFrame()

--- _assets/js/posts.js
+++ _assets/js/posts.js
@@ -2,31 +2,35 @@
 
 // Load more posts
 document.getElementById('moreposts').onclick = function () {
-  if (!/Mobi/.test(navigator.userAgent)) {
-    // Only apply styling for desktop browsers
-    this.previousElementSibling.style.overflowY = 'auto'
-    this.previousElementSibling.style.height = '576px'
-  }
-  const els = document.querySelectorAll('li[hidden]')
-  for (let i = 0, j = els.length; i < 4; i++) {
-    els[i].removeAttribute('hidden')
-    if (j === i + 1) {
-      this.parentNode.removeChild(this)
+  const button = this
+  const els = list.querySelectorAll('li[hidden]')
+  window.requestAnimationFrame(function () {
+    for (let i = 0, j = els.length; i < 4; i++) {
+      els[i].removeAttribute('hidden')
+      if (i + 1 === j) {
+        // Remove button when after last element done
+        button.parentNode.removeChild(button)
+      }
     }
-  }
+  })
 }
 
 // New posts badges
 let lastIndex = Number(window.localStorage.getItem('lastindex'))
-const els = document.querySelectorAll('[data-index]')
-for (let i = els.length - 1; i >= 0; i--) {
-  const index = Number(els[i].getAttribute('data-index'))
-  if (index > lastIndex) {
-    const span = document.createElement('span')
-    span.className = 'badge badge-primary ml-1'
-    span.textContent = 'Neu'
-    els[i].appendChild(span)
-    lastIndex = index
+const list = document.getElementById('posts')
+const els = list.querySelectorAll('li:not([hidden])')
+
+window.requestAnimationFrame(function () {
+  for (let i = els.length - 1; i >= 0; i--) {
+    const dataElement = els[i].querySelector('a[data-index]')
+    const index = Number(dataElement.getAttribute('data-index'))
+    if (index > lastIndex) {
+      const span = document.createElement('span')
+      span.className = 'badge badge-primary ml-1'
+      span.textContent = 'Neu'
+      dataElement.appendChild(span)
+      lastIndex = index
+    }
   }
-}
+})
 window.localStorage.setItem('lastindex', lastIndex)
