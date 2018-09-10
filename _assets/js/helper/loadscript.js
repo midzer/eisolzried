f165a19ea20d4a63497ecc809339222872c60cfb
midzer <midzer@gmail.com>
2018-09-10 10:50:42 +0200

    dont need promise to load script

--- _assets/js/helper/loadscript.js
+++ _assets/js/helper/loadscript.js
@@ -1,8 +1,18 @@
-export function loadScript (src) {
+export function loadScript (file) {
+  const script = document.createElement('script')
+  script.async = false
+  script.src = `/assets/js/${file}`
+  if (document.documentElement.lastChild.src !== script.src) {
+    document.documentElement.appendChild(script)
+  }
+}
+
+/*
+export function loadScript (file) {
   return new Promise((resolve, reject) => {
     const script = document.createElement('script')
     script.async = false
-    script.src = `/assets/js/${src}`
+    script.src = `/assets/js/${file}`
     script.onload = () => {
       resolve(script.src)
     }
@@ -12,7 +22,7 @@ export function loadScript (src) {
       document.documentElement.appendChild(script)
     }
   })
-}
+}*/
 
 //export function loadModule (src) {
 //  import(/* webpackIgnore: true */`./modules/${src}`)
