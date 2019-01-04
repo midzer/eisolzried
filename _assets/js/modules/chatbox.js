53cedc93e67646e5923513891c6dc3d734de32c1
midzer <midzer@gmail.com>
2019-01-04 15:18:04 +0100

    markdown support for chatbox

--- _assets/js/modules/chatbox.js
+++ _assets/js/modules/chatbox.js
@@ -5,9 +5,55 @@ loadStyle('chatbox.css')
 /* Can be import()ed dynamically in the future
  * if browser support is better
  */
+// Markdown
+function md2html(md){
+  const bold_pattern1 = /\*{2}(.+)\*{2}/gim // <b>
+  const bold_pattern2 = /\_{2}(.+)\_{2}/gim  // <b>
+  const italic_pattern1 = /\_(.+)\_/gim // <i>
+  const italic_pattern2 = /\*(.+)\*/gim // <i>
+  const striketrough_pattern = /\~{2}(.+)\~{2}/gim // <del>
+  const a_pattern1 = /\[(.+)\]\((.+)\)/gim // <a>
+  const a_pattern2 = /\[(.+)\]\((.+) \"(.+)\"\)/gim // <a>
+  const a_pattern3 = /\[(.+)\]/gim // <a>
+
+  /* links */
+  md = md.replace(a_pattern1, function(match, title, url) {
+    return '<a href="' + url + '">' + title + '</a>'
+  })
+  md = md.replace(a_pattern2, function(match, title, url, tooltip) {
+    return '<a href="' + url + '" title="' + tooltip + '">' + title + '</a>'
+  })
+  md = md.replace(a_pattern3, function(match, url) {
+    return '<a href="' + url + '">' + url + '</a>'
+  })
+
+  /* bold */
+  md = md.replace(bold_pattern1, function(match, str) {
+    return '<b>' + str + '</b>'
+  })
+  md = md.replace(bold_pattern2, function(match, str) {
+    return '<b>' + str + '</b>'
+  })
+
+  /* italic */
+  md = md.replace(italic_pattern1, function(match, str) {
+    return '<i>' + str + '</i>';
+  })
+  md = md.replace(italic_pattern2, function(match, str) {
+    return '<i>' + str + '</i>';
+  })
+
+  /* striketrough */
+  md = md.replace(striketrough_pattern, function(match, str) {
+    return '<del>' + str + '</del>'
+  })
+
+  return md
+}
+
 function addMessage (message) {
   const item = document.createElement('li')
-  item.textContent = message
+  item.innerHTML = md2html(message)
   messagesList.appendChild(item)
   chatbox.scrollTop = chatbox.scrollHeight
 }
