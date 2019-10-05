08aea795c0425b99ff78f32067494441f60106b6
midzer <midzer@gmail.com>
2019-10-05 14:54:11 +0200

    improve ARIA

--- _assets/js/lightbox.js
+++ _assets/js/lightbox.js
@@ -4,7 +4,7 @@ import { loadStyle } from './helper/loadstyle'
 loadStyle('lightbox.css')
 
 window.tobi = new Tobi({
-  navLabel: ['Vorheriges', 'Nächstes'],
+  navLabel: ['Vorheriges Bild', 'Nächstes Bild'],
   closeLabel: 'Schließen',
   counter: false,
   zoom: false,
