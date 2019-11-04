863503ec54449f8ab144b549eb487fb554564461
midzer <midzer@gmail.com>
2019-11-04 21:28:11 +0100

    remove promise for lightbox

--- _assets/js/lightbox.js
+++ _assets/js/lightbox.js
@@ -2,13 +2,12 @@ import Tobi from 'rqrauhvmra__tobi'
 import { loadStyle } from './load/loadstyle'
 
 loadStyle('lightbox.css')
-.then(() => {
-  window.tobi = new Tobi({
-    navLabel: ['Vorheriges Bild', 'Nächstes Bild'],
-    closeLabel: 'Schließen',
-    counter: false,
-    zoom: false,
-    autoplayVideo: true,
-    captions: false
-  })
+
+window.tobi = new Tobi({
+  navLabel: ['Vorheriges Bild', 'Nächstes Bild'],
+  closeLabel: 'Schließen',
+  counter: false,
+  zoom: false,
+  autoplayVideo: true,
+  captions: false
 })
