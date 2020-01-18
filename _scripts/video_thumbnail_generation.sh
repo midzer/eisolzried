eda398e51448432b7c8ed7cfcb2325be429b8157
midzer <midzer@gmail.com>
2020-01-18 16:04:29 +0100

    improve video thumbnail generation

--- _scripts/video_thumbnail_generation.sh
+++ _scripts/video_thumbnail_generation.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-for i in *.mp4; do ffmpeg -i "$i" -vf "scale='if(gt(dar,200/200),200*dar,200)':'if(gt(dar,200/200),200,200/dar)',setsar=1,crop=200:200" -frames:v 1 "thumbs/${i%.*}-1x.jpg"; done
-for i in *.mp4; do ffmpeg -i "$i" -vf "scale='if(gt(dar,400/400),400*dar,400)':'if(gt(dar,400/400),400,400/dar)',setsar=1,crop=400:400" -frames:v 1 "thumbs/${i%.*}-2x.jpg"; done
-for i in *.mp4; do ffmpeg -i "$i" -vf "scale='if(gt(dar,600/600),600*dar,600)':'if(gt(dar,600/600),600,600/dar)',setsar=1,crop=600:600" -frames:v 1 "thumbs/${i%.*}-3x.jpg"; done
+for i in *.mp4; do ffmpeg -i "$i" -vf "crop='min(200,min(iw,ih)):min(200,min(iw,ih))'" -frames:v 1 "thumbs/${i%.*}-1x.jpg"; done
+for i in *.mp4; do ffmpeg -i "$i" -vf "crop='min(400,min(iw,ih)):min(400,min(iw,ih))'" -frames:v 1 "thumbs/${i%.*}-2x.jpg"; done
+for i in *.mp4; do ffmpeg -i "$i" -vf "crop='min(600,min(iw,ih)):min(600,min(iw,ih))'" -frames:v 1 "thumbs/${i%.*}-3x.jpg"; done
