a27a7e4947315ca82e329b8d1c1f2e6215c236e3
midzer <midzer@gmail.com>
2021-01-13 10:17:20 +0100

    replace space with %20 in data URI

--- _assets/js/media/trans.js
+++ _assets/js/media/trans.js
@@ -1,1 +1,1 @@
-export const trans = "data:image/svg+xml,%3csvg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 200 200'%3e%3c/svg%3e"
+export const trans = "data:image/svg+xml,%3csvg%20xmlns='http://www.w3.org/2000/svg'%20viewBox='0%200%20200%20200'%3e%3c/svg%3e"
