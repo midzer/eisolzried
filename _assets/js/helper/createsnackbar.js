047edd8eb2f2727b3e8af2371ddbfa2fcba39a86
midzer <midzer@gmail.com>
2022-01-23 01:29:39 +0100

    clean whitespaces

--- _assets/js/helper/createsnackbar.js
+++ _assets/js/helper/createsnackbar.js
@@ -1,15 +1,15 @@
 export function createSnackbar () {
-    const snackbar = document.createElement('div'),
-      snackbarText = document.createElement('div'),
-      snackbarButton = document.createElement('button')
-    
-    snackbar.className = 'snackbar'
-    snackbarText.className = 'snackbar__text'
-    snackbarButton.className = 'snackbar__action'
-    snackbarButton.type = 'button'
-    
-    snackbar.appendChild(snackbarText)
-    snackbar.appendChild(snackbarButton)
-    
-    return new Snackbar(document.body.appendChild(snackbar))
+  const snackbar = document.createElement('div'),
+    snackbarText = document.createElement('div'),
+    snackbarButton = document.createElement('button')
+  
+  snackbar.className = 'snackbar'
+  snackbarText.className = 'snackbar__text'
+  snackbarButton.className = 'snackbar__action'
+  snackbarButton.type = 'button'
+  
+  snackbar.appendChild(snackbarText)
+  snackbar.appendChild(snackbarButton)
+  
+  return new Snackbar(document.body.appendChild(snackbar))
 }
